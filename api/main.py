ecbcc9a8cc3ce03b53a5bc6f09b4e3dec6ee43e8
Deshraj Yadav <deshrajdry@gmail.com>
2024-01-05 14:49:06 +0530

    Update command to launch app

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from routes import api, admin
@@ -8,3 +10,6 @@ app = FastAPI(title="Embedchain API")
 
 app.include_router(api.router)
 app.include_router(admin.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
