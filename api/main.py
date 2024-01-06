5e0521e4e504955e0b32a19b36a1d44e3aae1d0f
Deshraj Yadav <deshrajdry@gmail.com>
2024-01-06 12:43:50 +0530

    Update fastapi and uvicorn

--- api/main.py
+++ api/main.py
@@ -13,4 +13,4 @@ app.include_router(admin.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info",
-                reload=True, timeout_workers=600, timeout_keep_alive=600)
+                reload=True, timeout_keep_alive=600)
