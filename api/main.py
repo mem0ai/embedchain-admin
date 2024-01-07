1fbd2a18fb69b309bc41676de7e249d6c387ee94
Deshraj Yadav <deshrajdry@gmail.com>
2024-01-07 21:01:38 +0530

    Minor bug fixes in docker configuration

--- api/main.py
+++ api/main.py
@@ -12,5 +12,5 @@ app.include_router(api.router)
 app.include_router(admin.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info",
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info",
                 reload=True, timeout_keep_alive=600)
